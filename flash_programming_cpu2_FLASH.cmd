e0ae275517170973b5d0e67f5a546f30dc2da6a2
DESKTOP-TNHFT77\mm_la <t18321822460@hotmail.com>
2024-02-28 19:47:56 +0800

    cpu2的CMD内，存放Read和Write结构体的RAMGS0和RAMGS1起止位置一旦修改，cpu2的Write结构体便无法正确赋值， 重新修改CMD，使得不改变RAMGS0和RAMGS1的情况下，能够放下角度存储数组dataStoreCan和dataStoreSci

--- flash_programming_cpu2_FLASH.cmd
+++ flash_programming_cpu2_FLASH.cmd
@@ -49,11 +49,11 @@ PAGE 0:    /* Program Memory */
    BEGIN           	: origin = 0x080000, length = 0x000002
    RAMM0           	: origin = 0x000080, length = 0x000380
    RAMD0           	: origin = 0x00B000, length = 0x000800
-   RAMLS03          : origin = 0x008000, length = 0x002000
+   RAMLS03          : origin = 0x008000, length = 0x000400
 /*	RAMLS1          : origin = 0x008800, length = 0x000800
     RAMLS2          : origin = 0x009000, length = 0x000800
 	RAMLS3          : origin = 0x009800, length = 0x000800 */
-   RAMLS4      		: origin = 0x00A000, length = 0x000100
+   RAMLS4      		: origin = 0x008400, length = 0x000400
    RAMGS14          : origin = 0x01A000, length = 0x001000
    RAMGS15          : origin = 0x01B000, length = 0x001000
    RESET       		: origin = 0x3FFFC0, length = 0x000002
@@ -78,13 +78,13 @@ PAGE 1 :
 
    BOOT_RSVD       : origin = 0x000002, length = 0x00007E     /* Part of M0, BOOT rom will use this for stack */
    RAMM1           : origin = 0x000400, length = 0x000400     /* on-chip RAM block M1 */
-   RAMD1           : origin = 0x00C800, length = 0x000800
+   RAMD1           : origin = 0x00B800, length = 0x000800
 
 
-   RAMLS5          : origin = 0x00A100, length = 0x002700
+   RAMLS5          : origin = 0x008800, length = 0x002000
                    
-   RAMGS0          : origin = 0x00D800, length = 0x000400
-   RAMGS1          : origin = 0x00DC00, length = 0x000400
+   RAMGS0          : origin = 0x00C000, length = 0x001000
+   RAMGS1          : origin = 0x00D000, length = 0x001000
    CPU2TOCPU1RAM   : origin = 0x03F800, length = 0x000400
    CPU1TOCPU2RAM   : origin = 0x03FC00, length = 0x000400
 
